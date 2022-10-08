6cdd72fdb00ab4429bed978b1ccd606064bb6848
Stephen Michaels <stephen.michaels3333@gmail.com>
2022-10-08 13:04:29 -0400

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            //Allow user to input the radius
+            Console.WriteLine("What is the radius of your circle");
+            var radius = double.Parse(Console.ReadLine());
+            
+            //Calculate area pof the circle
+            var areaOfCircle = CalculateArea (radius);
+
+            Console.WriteLine($"The area of a circle with radius of 20 is {areaOfCircle}");
+
+            
         }
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+
+        }
+        
+        
+
+        
+
     }
 }
